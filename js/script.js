92e9d014a8c1e0f443518c717363f832076f8a3e
Alex Bondarenko <alexandr96b@gmail.com>
2019-04-25 20:26:01 +0300

    add section product,course and info

--- js/script.js
+++ js/script.js
@@ -13,6 +13,85 @@ $('.b-main-slider').slick({
 });
 
 
+$('.b-course-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: false,
+    variableWidth: true,
+    centerMode: true,
+    arrows: true,
+    infinity: true,
+    dots: false,
+    nextArrow: '<button class="b-arrow b-arrow__next">›</button>',
+    prevArrow: '<button class="b-arrow b-arrow__prev">‹</button>',    
+    responsive: [
+  {
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 992,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]
+});
+
+
+
+$('.b-product-slider > .row').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    fade: false,
+    variableWidth: false,
+    arrows: true,
+    dots: false,
+    nextArrow: '<button class="b-arrow b-arrow__next">›</button>',
+    prevArrow: '<button class="b-arrow b-arrow__prev">‹</button>',    
+    responsive: [
+  {
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 4,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 992,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]
+});
+
+
 
 $('.b-certificates-content > .row').slick({
     slidesToShow: 6,
